ceae4f83048e2e256f9fc52288ba1eba526b8132
Chris Vander Veen <cjvander@ualberta.ca>
2025-02-23 12:14:05 -0700

    updated hero

--- src/lib/Hero.svelte
+++ src/lib/Hero.svelte
@@ -1,10 +1,9 @@
-<div class="hero bg-base-200 min-h-screen flex items-center justify-center">
+<div class="hero bg-base-200 flex items-center justify-center" style="height: calc(100vh - 64px);">
     <div class="hero-content text-center w-full p-0">
       <div class="max-w-3xl mx-auto">
-        <h1 class="text-5xl font-regular whitespace-nowrap" style="font-family: 'IBM Plex Mono', monospace;">Hi, I'm <span class="text-neutral-500">Chris Vander Veen</span></h1>
+        <h1 class="text-5xl font-regular" style="font-family: 'IBM Plex Mono', monospace;">Hello, World! <br><span class="text-neutral-500">I'm Chris Vander Veen</span></h1>
         <p class="py-6" style="font-family: 'IBM Plex Mono', monospace;">
-          Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-          quasi. In deleniti eaque aut repudiandae et a id nisi.
+          Always learning, always building.
         </p>
         <button class="btn btn-primary" style="font-family: 'IBM Plex Mono', monospace;">Get Started</button>
       </div>

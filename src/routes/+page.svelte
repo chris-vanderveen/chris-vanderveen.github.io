3f7b0167e068bf44f6f7c606505639605bc8b606
Chris Vander Veen <cjvander@ualberta.ca>
2025-02-22 17:03:37 -0700

    fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,3 +3,19 @@
 </script>
 
 <Navbar />
+
+<section id="about">
+  <h2>About</h2>
+</section>
+
+<section id="experience">
+  <h2>Experience</h2>
+</section>
+
+<section id="projects">
+  <h2>Projects</h2>
+</section>
+
+<section id="contact">
+  <h2>Contact</h2>
+</section>
